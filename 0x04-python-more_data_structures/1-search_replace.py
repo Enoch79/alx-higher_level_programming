17a6d7d40ea93a8309b8faf7c2d72bf59464ee73
Enoch79 <linanar8@gmail.com>
2022-06-08 17:41:08 -0700

    search and replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+ #!/usr/bin/python3
+ def search_replace(my_list, search, replace):
+     copy = []
+     for i in range(len(my_list)):
+         if my_list[i] == search:
+             copy.append(replace)
+         else:
+             copy.apeend(my_list[i])
+             return copy
