c9f1421b7424a7bba3066a6ff791903052cb816a
Enoch79 <linanar8@gmail.com>
2022-09-22 22:14:37 -0700

    python network

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-# Sends a GET request to that URL, and displays the body of the reponse
+# Sends a GET request to that URL, and displays the body of the response
 curl -sH "X-herbertonSchool-User-Id: 98" "${1}"
